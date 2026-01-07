080a2fa13cdb06f816e462a87ef60560cf6ceffd
ramijujuy <ramijujuy@gmail.com>
2026-01-07 12:15:59 -0300

    conexion a bae

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,7 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
-
 const connectDB = async () => {
-  if (isConnected) return;
-
-  if (!process.env.MONGO_URI) {
-    throw new Error("MONGO_URI no definida");
-  }
-
   await mongoose.connect(process.env.MONGO_URI);
-  isConnected = true;
   console.log("MongoDB conectado");
 };
 
