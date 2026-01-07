3bc4333e199a4ec2e1e200c86cad26656eca8119
ramijujuy <ramijujuy@gmail.com>
2026-01-07 17:24:49 -0300

    solucion permisos

--- src/routes/personRoutes.js
+++ src/routes/personRoutes.js
@@ -15,16 +15,16 @@ router.use(protect);
 
 router
   .route("/")
-  .get(authorize("admin", "administrativo"), getPersons)
-  .post(authorize("admin", "administrativo"), createPerson);
+  .get(authorize("admin", "administrativo", "staff"), getPersons)
+  .post(authorize("admin", "administrativo", "staff"), createPerson);
 router
   .route("/:id")
-  .get(authorize("admin", "administrativo"), getPerson)
-  .put(authorize("admin", "administrativo"), updatePerson)
+  .get(authorize("admin", "administrativo", "staff"), getPerson)
+  .put(authorize("admin", "administrativo", "staff"), updatePerson)
   .delete(authorize("admin"), archivePerson);
 
 router
   .route("/:id/group")
-  .put(authorize("admin", "administrativo"), updatePersonGroup);
+  .put(authorize("admin", "administrativo", "staff"), updatePersonGroup);
 
 module.exports = router;
